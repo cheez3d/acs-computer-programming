acbde422c31718e37ced52d898c1c09b048c2ec5
Andrei Stoian <andrei.stoian0307@stud.acs.upb.ro>
2018-12-22 19:17:44 +0200

    Added .vscode Updated .gitignore Updated Makefile

--- tema-1/src/helpers.c
+++ tema-1/src/helpers.c
@@ -16,9 +16,12 @@ unsigned long long gcd(unsigned long long a, unsigned long long b) {
 int compareDescending(const void *const a, const void *const b) {
     int res = 0;
 
-    if (*(unsigned long long*)a > *(unsigned long long*)b) {
+    const unsigned long long left = *(const unsigned long long *const)a;
+    const unsigned long long right = *(const unsigned long long *const)b;
+
+    if (left > right) {
         res = -1;
-    } else if (*(unsigned long long*)a < *(unsigned long long*)b) {
+    } else if (left < right) {
         res = 1;
     }
 
